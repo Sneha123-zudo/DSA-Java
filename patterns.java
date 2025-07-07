55942bbfd51e0e76b64c82c7f192e1ab3de42e4b
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-07-08 00:59:09 +0530

    patterns questions

--- patterns.java
+++ patterns.java
@@ -32,12 +32,37 @@ public class patterns{
 //System.out.println(" ");
 //}
 
-for(int i=4 ; i>=1; i--){
-    for(int j=1; j<=i; j++){
-        System.out.print("*");
+//for(int i=4 ; i>=1; i--){
+//    for(int j=1; j<=i; j++){
+//        System.out.print("*");
+//    }
+//    System.out.println();
+//}
+
+//for(int i=1 ; i<=4; i++){
+//    for(int j = 1; j<=4-i; j++){
+//        System.out.print(" ");
+//    }
+//        for(int j = 1; j<=i; j++){
+//        System.out.print("*");
+//    }
+//    System.out.println();
+//}
+
+//for(int i = 1; i<=5; i++){
+//    for(int j=1; j<=i; j++){
+//        System.out.print(j);
+//    }
+//    System.out.println();
+//}
+
+for(int i = 5; i>=1; i--){
+    for(int j=1; j<=i;j++){
+        System.out.print(j + " " );
     }
     System.out.println();
 }
+
 
  }
 }
