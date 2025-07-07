f66c1ea0ee8860e0908ea7361ae3048b59d046dd
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-07-07 13:25:56 +0530

    one more pattern qs

--- patterns.java
+++ patterns.java
@@ -25,11 +25,18 @@ public class patterns{
 //        }
     
 
-for(int i=1; i<=4; i++){
-    for(int j=1; j<=i ; j++) {
+//for(int i=1; i<=4; i++){
+//    for(int j=1; j<=i ; j++) {
+//        System.out.print("*");
+//    } 
+//System.out.println(" ");
+//}
+
+for(int i=4 ; i>=1; i--){
+    for(int j=1; j<=i; j++){
         System.out.print("*");
-    } 
-System.out.println(" ");
+    }
+    System.out.println();
 }
 
  }
