6acc98b7118a98e133d67955a18850d9fb96b239
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-07-08 13:43:51 +0530

    patterns

--- patterns.java
+++ patterns.java
@@ -56,13 +56,106 @@ public class patterns{
 //    System.out.println();
 //}
 
-for(int i = 5; i>=1; i--){
-    for(int j=1; j<=i;j++){
-        System.out.print(j + " " );
+//for(int i = 5; i>=1; i--){
+//    for(int j=1; j<=i;j++){
+//        System.out.print(j + " " );
+//    }
+//    System.out.println();
+//}
+
+//int num = 1;
+//for(int i = 1; i<=5; i++){
+//    for(int j=1; j<=i;j++){
+//        System.out.print(num + " ");
+//        num++;
+//    }
+//    System.out.println();
+//}
+
+//for(int i = 1; i<=5; i++){
+//    for(int j=1; j<=i;j++){
+//        if((i+j)%2==0){
+//            System.out.print("1 ");
+//        }
+//        else{
+//            System.out.print("0 ");
+//
+//        }
+//    }
+//    System.out.println();
+//}
+
+//for(int i =1; i<=5; i++){
+//
+//    for(int j = 1; j<=5-i; j++){
+//        System.out.print(" ");
+//    }
+//    for(int j =1 ;j<=5; j++){
+//               if(i==1 || j==1 || i==5 || j==5){
+//                   System.out.print("*");
+//               }
+//               else{
+//                   System.out.print(" ");
+//               }
+//           }
+//
+//    System.out.println(" ");
+//}
+
+
+
+//for(int i =1; i<=5; i++){
+//
+//    for(int j = 1; j<=5-i; j++){
+//        System.out.print(" ");
+//    }
+//    for(int j =1 ;j<=i; j++){
+//        System.out.print(i + " ");
+//           }
+//
+//    System.out.println(" ");
+//}
+
+
+//for(int i =1; i<=5; i++){
+//
+//    for(int j = 1; j<=5-i; j++){
+//        System.out.print(" ");
+//    }
+//    for(int j =i ;j>=1; j--){
+//        System.out.print(j);
+//    }
+//    for(int j =2 ;j<=i; j++){
+//        System.out.print(j);
+//    }
+//
+//    System.out.println(" ");
+//}
+
+
+for(int i =1; i<=5; i++){
+
+    for(int j = 1; j<=5-i; j++){
+        System.out.print(" ");
     }
-    System.out.println();
-}
+    for(int j =1 ;j<=2*i-1; j++){
+        System.out.print("*");
+           }
 
+    System.out.println(" ");
 
+    }
+    for(int i =5; i>=1; i--){
+
+    for(int j = 1; j<=5-i; j++){
+        System.out.print(" ");
+    }
+    for(int j =1 ;j<=2*i-1; j++){
+        System.out.print("*");
+           }
+
+    System.out.println(" ");
+
+    }
  }
 }
