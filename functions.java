7b45c88423466423cda5dcf189a2857d4277a46a
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-08-18 21:47:19 +0530

    functions

--- functions.java
+++ functions.java
@@ -1,17 +1,33 @@
 
 
 
+//import java.util.*;
+//
+//public class functions{
+//    public static void printMyName(String name){
+//        System.out.println(name);
+//        return;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.nextLine();
+//        printMyName(name);
+//
+//    }
+//}
+
+
 import java.util.*;
 
 public class functions{
-    public static void printMyName(String name){
-        System.out.println(name);
-        return;
+    public static int CalculateSum(int a, int b){
+       int sum = a + b;
+       System.out.println(sum);
+       return a + b;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printMyName(name);
-
-    }
-}
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        CalculateSum(a, b);
+    }}
