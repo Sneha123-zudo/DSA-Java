e0453a81957c7cfd62b15504b807b59b9b5b301e
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-09-07 18:56:26 +0530

    solved some problems related to numbers

---
+++ Numbers/reversenum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class reversenum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int rev = 0;
+        while(n>0){
+            rev = (rev*10)+n%10;
+            n=n/10;
+        }
+        System.out.println("Reversed number:" + rev);
+      
+        }
+             
+    }
