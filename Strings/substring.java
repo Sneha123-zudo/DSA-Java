ad5d47cad9cb838bc9e834f4f1a618c705b367eb
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-09-11 01:42:48 +0530

    strings

---
+++ Strings/substring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class substring{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter a String: ");
+        String str = sc.nextLine();
+        for(int i = 0; i<=3;i++){
+            for(int j = i+1; j<=4; j++){
+                System.err.print(str.substring(i,j) + " ");
+            }
+        }
+
+        
+    }}
