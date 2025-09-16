9307ee997a9e5e5bde41ed0dc11124feae8b7e0d
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-09-16 21:17:20 +0530

    chnages

--- codingcheatsheet/question14.java
+++ codingcheatsheet/question14.java
@@ -18,6 +18,8 @@ public class question14{
             n = n*-1;
             for(int i = 1; i<n; i++){
                 if(n%i==0){
+
+                    
                     System.out.print(i + " ");
                 }
             }
